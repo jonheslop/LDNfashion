1c8d3eaa3a29e31021f8969a3302be644cdcce55
Jon Heslop <jon@jonheslop.com>
2014-04-05 19:56:21 +0100

    Added jump links

--- page-brands.php
+++ page-brands.php
@@ -11,31 +11,43 @@
 ?>
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); ?>
 
+<?php $args = array(
+  'hide_empty' => false,
+  'parent' => 0
+  );
+  $letter=' ';
+  $brands = get_terms( 'brand', $args ); ?>
 <section role="main" class="container">
   <section id="content" class="cf">
     <div class="wrapper partition page_headline">
       <header class="section_header post-header">
         <h2>Browse by brand</h2>
       </header>
+        <p class="letter-list">Jump to:
+          <?php foreach ($brands as $brand) :
+          $name = $brand->name;
+          $initial = strtoupper(substr($name, 0,1));
+          if($initial!=$letter) : ?>
+            <a href="#<?= $initial; ?>"><?= $initial; ?></a>
+          <? $letter=$initial; 
+          endif;
+          endforeach; ?>
+        </p>
     </div>
     <ul class="cf wrapper brands-alpha">
-    <?php $args = array(
-        'hide_empty' => false,
-        'parent' => 0
-    );
-      $letter=' ';
-      $brands = get_terms( 'brand', $args ); ?>
       <?php foreach ($brands as $brand) :
         $name = $brand->name;
         $initial = strtoupper(substr($name, 0,1)); ?>
-        <li>
         <? if($initial!=$letter) {
+          echo '<li id="' . $initial . '">';
           echo '<header class="section_header"><h3>' . $initial . '</h3></header>';
           $letter=$initial;
         } ?>
           <p><a href="/brands/<?= $brand->slug; ?>"><?= $brand->name; ?></a></p>
-        </li>
-      <? endforeach; ?>
+        <? if($initial!=$letter) {
+        echo '</li>';
+      }
+      endforeach; ?>
   </section>
   <?php get_template_part( 'parts/_sidebar' ); ?>
 </section>

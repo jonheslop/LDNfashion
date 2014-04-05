6fb4203612a0bc3179dd966a47aac12132bdda44
Jon Heslop <jon@jonheslop.com>
2014-04-05 14:01:24 +0100

    Updates and stuff. Fixed the nav on scroll

--- js/site.js
+++ js/site.js
@@ -1,11 +1,26 @@
 
-	jQuery(document).ready(function($) {
+jQuery(document).ready(function($) {
 
-		$('.voucher-code-button').click(function(e){
-			vouchercode = $(this).data('voucher-code');
-			$(this).replaceWith('<input class="voucher-code-button" value="' + vouchercode + '" disabled="disabled" />');
-			// $(this).text(vouchercode);
-		});
-
+	$('.voucher-code-button').click(function(e){
+		vouchercode = $(this).data('voucher-code');
+		$(this).replaceWith('<input class="voucher-code-button" value="' + vouchercode + '" disabled="disabled" />');
+		// $(this).text(vouchercode);
 	});
+
+	navOffset = Math.round($('nav[role="navigation"]').offset().top);
+	navHeight = $('nav[role="navigation"]').outerHeight();
+	console.log(navOffset);
+
+	$(window).scroll(function(){
+		scrollPosition = $(window).scrollTop();
+		console.log(scrollPosition);
+		if ( scrollPosition >= navOffset ) {
+			console.log('its happening!');
+			$('body').addClass('fixed').css('margin-top',navHeight);
+		} else {
+			$('body').removeClass('fixed').css('margin-top',0);
+		}
+	});
+
+});
 

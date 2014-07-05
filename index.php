ad18e1d6c79d2e6b4a9138815a0170a10bca8720
Jon Heslop <jon@jonheslop.com>
2014-07-05 23:34:06 +0100

    Index removed category listing

--- index.php
+++ index.php
@@ -140,33 +140,6 @@
 	</section>
 	<?php endif; ?>
 	<?php endif; ?>
-	<?php if ( have_posts() ) : ?>
-	<?php query_posts($query_string.'&cat=4962'); ?>
-		<section class="partition wrapper cf">
-			<header class="section_header sidebar_header">
-				<h4>Menswear News</h4>
-			</header>
-		</section>
-		<ul class="posts cf equalHeights">
-			<?php while ( have_posts() ) : the_post();
-				$imageID = get_post_thumbnail_id($post->ID);
-				$image = wp_get_attachment_image_src($imageID, 'index-thumb');?>
-			<li class="post-thumb wrapper cf">
-			<a href="<?php the_permalink(); ?>">
-				<?php if ( $image ) : ?>
-					<figure class="post-image">
-						<img src="<?php echo $image[0]; ?>">
-					</figure>
-				<?php endif; ?>
-					<header class="section_header post-thumb-header">
-						<h4><?php the_title(); ?></h4>
-					</header>
-				</a>
-			</li>
-		<?php endwhile; ?>
-		</ul>
-	</section>
-	<?php endif; ?>
 	<?php get_template_part( 'parts/_sidebar' ); ?>
 </section>
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/footer','parts/shared/html-footer') ); ?>

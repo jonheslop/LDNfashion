7d5c94bc9b50ec929d1439aebdddfaa3bd439183
Jon Heslop <jon@jonheslop.com>
2014-07-31 23:48:34 +0100

    fixed nav on category pages

--- parts/shared/header.php
+++ parts/shared/header.php
@@ -2,7 +2,14 @@
 	<div class="wrapper logo_wrap">
 		<h1><a id="logo" href="<?php echo home_url(); ?>"><?php bloginfo( 'name' ); ?></a></h1>
 	</div>
-	<?php wp_nav_menu( array( 'theme_location' => 'primary-nav' ) ); ?>
+	<?php wp_nav_menu( array( 'theme_location' => 'primary-nav' ) );
+	if ( is_category() ) {
+		$backup_query = $wp_query;
+		$wp_query = new WP_Query(array('post_type' => 'post'));
+		wp_nav_menu( array( 'theme_location' => 'primary-nav' ) ); 
+		$wp_query = $backup_query;
+	}
+	?>
 </nav>
 <header role="banner" class="container">
 	<?php if (function_exists('dynamic_sidebar') && dynamic_sidebar('Header Banner')); ?>

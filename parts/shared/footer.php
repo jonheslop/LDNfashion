ba37080173d6606b885dc972bcd4d154da052c47
Jon Heslop <jon@jonheslop.com>
2014-08-01 01:17:31 +0100

    Footer titles

--- parts/shared/footer.php
+++ parts/shared/footer.php
@@ -2,21 +2,39 @@
 		<div class="container">
 			<section class="footer_section wrapper">
 				<header class="section_header footer_header">
-					<h4>About us</h4>
+					<h4>Links</h4>
 				</header>
 				<?php wp_nav_menu( array( 'theme_location' => 'footer-nav-1' ) ); ?>
+				<?php if ( is_home() ) {
+					$backup_query = $wp_query;
+					$wp_query = new WP_Query(array('post_type' => 'post'));
+					wp_nav_menu( array( 'theme_location' => 'footer-nav-1' ) ); 
+					$wp_query = $backup_query;
+				} ?>
 			</section>
 			<section class="footer_section wrapper">
 				<header class="section_header footer_header">
-					<h4>About us</h4>
+					<h4>Masthead</h4>
 				</header>
 				<?php wp_nav_menu( array( 'theme_location' => 'footer-nav-2' ) ); ?>
+				<?php if ( is_home() ) {
+					$backup_query = $wp_query;
+					$wp_query = new WP_Query(array('post_type' => 'post'));
+					wp_nav_menu( array( 'theme_location' => 'footer-nav-2' ) ); 
+					$wp_query = $backup_query;
+				} ?>
 			</section>
 			<section class="footer_section wrapper double">
 				<header class="section_header footer_header">
-					<h4>About us</h4>
+					<h4>London Department Stores</h4>
 				</header>
 				<?php wp_nav_menu( array( 'theme_location' => 'footer-nav-3' ) ); ?>
+				<?php if ( is_home() ) {
+					$backup_query = $wp_query;
+					$wp_query = new WP_Query(array('post_type' => 'post'));
+					wp_nav_menu( array( 'theme_location' => 'footer-nav-3' ) ); 
+					$wp_query = $backup_query;
+				} ?>
 			</section>
 			<p class="credits wrapper">&copy; <?php echo date("Y"); ?> <?php bloginfo( 'name' ); ?>. All rights reserved.</p>
 		</div>

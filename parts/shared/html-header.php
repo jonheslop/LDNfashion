0b7e17bd03629bf67c8ea45e7f5441dace68bfde
Jon Heslop <jon@jonheslop.com>
2014-08-10 13:14:24 +0100

    Added favicon

--- parts/shared/html-header.php
+++ parts/shared/html-header.php
@@ -10,7 +10,7 @@
 	  	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0"><!-- Remove if you're not building a responsive site. (But then why would you do such a thing?) -->
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-		<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon.ico"/>
+		<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico"/>
 		<script type="text/javascript" src="//use.typekit.net/hbj0cwk.js"></script>
 		<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
 		<?php wp_head(); ?>

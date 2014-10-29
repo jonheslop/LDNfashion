cd0a890f81d9d18486ddabeb93e0fc807cf8bf8e
Jon Heslop <jon@jonheslop.com>
2014-10-29 23:16:49 +0000

    printing tax meta

--- page-48351.php
+++ page-48351.php
@@ -37,7 +37,9 @@
     <ul class="cf wrapper brands-alpha">
       <?php foreach ($brands as $brand) :
         $name = $brand->name;
-        $initial = strtoupper(substr($name, 0,1)); ?>
+        $initial = strtoupper(substr($name, 0,1));
+        $saved_data = get_tax_meta($brand->id,'exclude_from_sample_sales');
+        echo $saved_data; ?>
         <? if($initial!=$letter) {
           echo '<li id="' . $initial . '">';
           echo '<header class="section_header"><h3>' . $initial . '</h3></header>';

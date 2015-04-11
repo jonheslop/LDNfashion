26e487d81cc8d90ce63368362f05b3640b036e43
Jon Heslop <jon@jonheslop.com>
2015-04-11 22:37:22 +0100

    4th footer element

--- parts/shared/footer.php
+++ parts/shared/footer.php
@@ -36,6 +36,18 @@
 					$wp_query = $backup_query;
 				} ?>
 			</section>
-			<p class="credits wrapper">&copy; <?php echo date("Y"); ?> <?php bloginfo( 'name' ); ?>. All rights reserved.</p>
+<!-- 			<section class="footer_section wrapper">
+				<header class="section_header footer_header">
+					<h4>XXX</h4>
+				</header>
+				<?php wp_nav_menu( array( 'theme_location' => 'footer-nav-4' ) ); ?>
+				<?php if ( is_home() || is_front_page() || is_category() ) {
+					$backup_query = $wp_query;
+					$wp_query = new WP_Query(array('post_type' => 'post'));
+					wp_nav_menu( array( 'theme_location' => 'footer-nav-4' ) ); 
+					$wp_query = $backup_query;
+				} ?>
+			</section>
+ -->			<p class="credits wrapper">&copy; <?php echo date("Y"); ?> <?php bloginfo( 'name' ); ?>. All rights reserved.</p>
 		</div>
 	</footer>

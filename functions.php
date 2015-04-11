df50e61bfdf96b11e1f6547957a2fd02e3f20e9b
Jon Heslop <jon@jonheslop.com>
2015-04-11 21:42:31 +0100

    fixed css dec

--- functions.php
+++ functions.php
@@ -103,7 +103,7 @@
 		wp_register_script( 'site', get_template_directory_uri().'/js/site.js', array( 'jquery' ), rand(), true );
 		wp_enqueue_script( 'site' );
 
-		wp_register_style( 'screen', get_stylesheet_directory_uri().'/style.css', '', '1.2', 'screen' );
+		wp_register_style( 'screen', get_stylesheet_directory_uri().'/style.css', '', '1.3', 'screen' );
 		wp_enqueue_style( 'screen' );
 	}	
 

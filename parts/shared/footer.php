b33ed20fe21958ce53e5894453f52210f2ffeaa3
Jon Heslop <jon@jonheslop.com>
2015-06-02 23:02:03 +0100

    Fixed dup footer

--- parts/shared/footer.php
+++ parts/shared/footer.php
@@ -7,39 +7,15 @@
 		<div class="container">
 			<section class="footer_section wrapper">
 				<?php wp_nav_menu( array( 'theme_location' => 'footer-nav-1' ) ); ?>
-				<?php if ( is_home() || is_front_page() || is_category() ) {
-					$backup_query = $wp_query;
-					$wp_query = new WP_Query(array('post_type' => 'post'));
-					wp_nav_menu( array( 'theme_location' => 'footer-nav-1' ) ); 
-					$wp_query = $backup_query;
-				} ?>
 			</section>
 			<section class="footer_section wrapper">
 				<?php wp_nav_menu( array( 'theme_location' => 'footer-nav-2' ) ); ?>
-				<?php if ( is_home() || is_front_page() || is_category() ) {
-					$backup_query = $wp_query;
-					$wp_query = new WP_Query(array('post_type' => 'post'));
-					wp_nav_menu( array( 'theme_location' => 'footer-nav-2' ) ); 
-					$wp_query = $backup_query;
-				} ?>
 			</section>
 			<section class="footer_section wrapper">
 				<?php wp_nav_menu( array( 'theme_location' => 'footer-nav-3' ) ); ?>
-				<?php if ( is_home() || is_front_page() || is_category() ) {
-					$backup_query = $wp_query;
-					$wp_query = new WP_Query(array('post_type' => 'post'));
-					wp_nav_menu( array( 'theme_location' => 'footer-nav-3' ) ); 
-					$wp_query = $backup_query;
-				} ?>
 			</section>
 			<section class="footer_section wrapper">
 				<?php wp_nav_menu( array( 'theme_location' => 'footer-nav-4' ) ); ?>
-				<?php if ( is_home() || is_front_page() || is_category() ) {
-					$backup_query = $wp_query;
-					$wp_query = new WP_Query(array('post_type' => 'post'));
-					wp_nav_menu( array( 'theme_location' => 'footer-nav-4' ) ); 
-					$wp_query = $backup_query;
-				} ?>
 			</section>
 			<p class="credits wrapper">&copy; <?php echo date("Y"); ?> <?php bloginfo( 'name' ); ?>. All rights reserved.</p>
 		</div>

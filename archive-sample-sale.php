77e2d0927e5238f8ed31e446287e2d78fa0a4f80
Jon Heslop <jon@jonheslop.com>
2015-06-02 21:35:23 +0100

    latest sales

--- archive-sample-sale.php
+++ archive-sample-sale.php
@@ -15,6 +15,33 @@
 	$letter=' '; ?>
 <section role="main" class="container">
 	<section id="content" class="cf">
+    <section class="partition wrapper cf">
+      <header class="section_header sidebar_header">
+        <h4>Latest Sample Sales</h4>
+      </header>
+      <ul class="posts cf equalHeights">
+        <?php while ( have_posts() ) : the_post();
+          $imageID = get_post_thumbnail_id($post->ID);
+          $image = wp_get_attachment_image_src($imageID, 'index-thumb');
+          $sample_sale_when = get_post_meta($post->ID, 'sample_sale_when', true); ?>
+      <? if ( $sample_sale_when ) : ?>
+        <li class="post-thumb wrapper cf">
+        <a href="<?php the_permalink(); ?>">
+          <?php if ( $image ) : ?>
+            <figure class="post-image">
+              <img src="<?php echo $image[0]; ?>">
+            </figure>
+          <?php endif; ?>
+            <header class="section_header post-thumb-header">
+              <h4><?php the_title(); ?></h4>
+            </header>
+          </a>
+        </li>
+      <?php endif; ?>
+      <?php endwhile; ?>
+      </ul>
+    </section>
+
     <div class="wrapper partition page_headline">
       <header class="section_header post-header">
         <h2>Find sample sales by brand</h2>

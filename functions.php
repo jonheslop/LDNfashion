0f347fd7112b57c090f3e867f43d579510212ddf
Jon Heslop <jon@jonheslop.com>
2015-08-03 22:28:11 +0100

    Fixing bug with nav not showing on cat pages

--- functions.php
+++ functions.php
@@ -269,3 +269,16 @@
 		}
 	}
 }
+
+add_filter('pre_get_posts', 'query_post_type');
+function query_post_type($query) {
+  if(is_category() || is_tag()) {
+    $post_type = get_query_var('post_type');
+	if($post_type)
+	    $post_type = $post_type;
+	else
+	    $post_type = array('nav_menu_item','post','articles');
+    $query->set('post_type',$post_type);
+	return $query;
+    }
+}
